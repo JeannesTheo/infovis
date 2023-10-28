528a4a3c9c2b3715c9ce556a48755a2ebeaaca62
AmbreCorreia <74357454+AmbreCorreia@users.noreply.github.com>
2023-10-28 22:56:16 +0200

    parallel set filtre

--- dataHandling/parallel_set_preprocess.R
+++ dataHandling/parallel_set_preprocess.R
@@ -2,15 +2,15 @@ library(dplyr)
 library(utils)
 library(rlang)
 library(stringr)
-base_dir <- "/home/theojeannes/Dev/Scolaire/InfoVis/git/dataHandling"
+base_dir <- "C:/Users/Ambre_Correia/DataspellProjects/infovis/dataHandling"
 setwd(base_dir)
 
 split_songs <- function() {
   setwd('data')
   #Step 1: Read the original CSV file into a data frame
-  data <- read.csv("songs_filtered_dates.csv")
-  # Step 2: Select the columns you want to keep
-  songs <- subset(data, select = c(explicitLyrics, artist_name, id_album))  # Exclude "Column1" and "Column2"
+  data <- read.csv("songs_cleaned_date_genre.csv")
+  # Step 2: Selec the columns you want to keep
+  songs <- subset(data, select = c(explicitLyrics, artist_name, id_album, genre))  # Exclude "Column1" and "Column2"
   # Step 3: Write the filtered data to a new CSV file
   #write.csv(songs, "parallel_set_songs.csv", row.names = FALSE)
   setwd(base_dir)
@@ -22,7 +22,7 @@ split_artists <- function() {
   #Step 1: Read the original CSV file into a data frame
   data <- read.csv("artists.csv")
   # Step 2: Select the columns you want to keep
-  artists <- subset(data, select = c(name, type, gender, location.country))  # Exclude "Column1" and "Column2"
+  artists <- subset(data, select = c(name, type, gender))
   colnames(artists)[colnames(artists) == "name"] <- "artist_name"
   # Step 3: Write the filtered data to a new CSV file
   #write.csv(artists, "parallel_set_artists.csv", row.names = FALSE)
@@ -49,47 +49,22 @@ join_data <- function(songs, artists, albums) {
   joined_data <- inner_join(songs, artists, by = "artist_name")
   joined_data_2 <- inner_join(joined_data, albums, by = "id_album")
   final <- subset(joined_data_2, select = -c(id_album, artist_name))
-  write.csv(final, "final.csv", row.names = FALSE)
+  #write.csv(final, "final.csv", row.names = FALSE)
   setwd(base_dir)
   return(final)
 }
 
-count_data <- function(final) {
+delete_and_fill_lines <- function(final, save_file) {
   setwd('data')
-  #data <- read.csv("parallel_set_songs.csv")
-  # Step 2: Use dplyr to group and count unique combinations
-  combinations_count <- final %>%
-    group_by_all() %>%
-    summarise(count = n())
-  # Step 3: Write the results to a CSV file
-  #write.csv(combinations_count, "parallel_set.csv", row.names = FALSE)
-  write.csv(final, "parallel_set2", row.names = FALSE)
-  setwd(base_dir)
-}
-
-count_without_country <- function() {
-  setwd('data')
-  final <- read.csv("final.csv")
-  splited <- subset(final, select = -c(location.country))
-  combinations_count <- splited %>%
-    group_by_all() %>%
-    summarise(count = n())
-  # Step 3: Write the results to a CSV file
-  write.csv(combinations_count, "parallel_set_filtered.csv", row.names = FALSE)
-  setwd(base_dir)
-}
-
-delete_and_fill_lines <- function(input_file, save_file) {
-  setwd('data')
-  final <- read.csv(input_file, header = TRUE, sep = ',', fill = TRUE, dec = '.')
+  #final <- read.csv(input_file, header = TRUE, sep = ',', fill = TRUE, dec = '.')
   #supprimer toutes les lignes qui n'ont pas de pays
   list_countries_empty <- sapply(final$location.country, is_empty_string)
   data <- final[!list_countries_empty,]
   # Spécifiez les colonnes que vous voulez vérifier pour les valeurs manquantes
   colonnes_a_verifier <- c("type", "gender", "explicitAlbum")
   # # Supprimez les lignes contenant des valeurs manquantes dans TOUTES les colonnes spécifiées
-  list_all_empty <- apply(data[, colonnes_a_verifier], 1, function(x) all(is_empty_string(x)))
-  data <- data[!list_all_empty,]
+  list_all_empty <- apply(final[, colonnes_a_verifier], 1, function(x) all(is_empty_string(x)))
+  data <- final[!list_all_empty,]a
   data$type <- mapply(function(x, y) {
     ifelse(is_empty_string(x),
            ifelse(y == "Male" || y == "Female", "Person", "Group"),
@@ -107,21 +82,30 @@ delete_and_fill_lines <- function(input_file, save_file) {
   # Sauvegardez le résultat dans un nouveau fichier CSV si nécessaire
   write.csv(data, save_file, row.names = FALSE)
   setwd(base_dir)
+  return (data)
 }
 
-is_empty_string <- function(x) return(is_null(x) || is_na(x) || is_empty(x) || x == "")
+count_data <- function(final) {
+  setwd('data')
+  #data <- read.csv("parallel_set_songs.csv")
+  # Step 2: Use dplyr to group and count unique combinations
+  combinations_count <- final %>%
+    group_by_all() %>%
+    summarise(value = n())
+  # Step 3: Write the results to a CSV file
+  write.csv(combinations_count, "parallel_set.csv", row.names = FALSE)
+  setwd(base_dir)
+}
 
-print('Start')
-delete_and_fill_lines(input_file='final.csv',save_file="filled_parallel_set.csv")
+is_empty_string <- function(x) return(is_null(x) | is_na(x) | is_empty(x) | x == "")
+
+
+songs <- split_songs()
+artists <- split_artists()
+albums <- split_album()
+final <- join_data(songs, artists, albums)
+print('Start fill')
+filled <- delete_and_fill_lines(final,save_file="filled_parallel_set.csv")
 print('Done')
-#songs <- split_songs()
-#artists <- split_artists()
-#albums <- split_album()
-#final <- join_data(songs, artists, albums)
-#count_data(final)
-#count_without_country()
-
-#setwd('data')
-#data <- read.csv("parallel_set2.csv")
-#f <- subset(data, select = -c(location.country))
-#write.csv(f, "parallel_set3.csv", row.names = FALSE)
+print('Start count')
+count_data(filled)
--- visualisation/public/parallel_sets/parallel_set.js
+++ visualisation/public/parallel_sets/parallel_set.js
@@ -1,187 +1,182 @@
-onload = function () {
-    const graph = (data) => {
-        console.log(data)
-        const keys = data.columns.slice(0, -1);
-        let index = -1;
-        const nodes = [];
-        const nodeByKey = new Map();
-        const indexByKey = new Map();
-        const links = [];
+window.onload = function () {
+    function createCheckboxes(genres) {
+        const genreDropdown = document.getElementById('genre-dropdown');
 
-        for (const k of keys) {
-            for (const d of data) {
-                const key = [k, d[k]];
-                if (!nodeByKey.has(JSON.stringify(key))) {
-                    const node = {name: d[k]};
-                    nodes.push(node);
-                    nodeByKey.set(JSON.stringify(key), node);
-                    indexByKey.set(JSON.stringify(key), ++index);
-                }
-            }
-        }
-
-        for (let i = 1; i < keys.length; ++i) {
-            const a = keys[i - 1];
-            const b = keys[i];
-            const prefix = keys.slice(0, i + 1);
-            const linkByKey = new Map();
-            for (const d of data) {
-                const names = prefix.map(k => d[k]);
-                const value = d.value || 1;
-                if (linkByKey.has(JSON.stringify(names))) {
-                    linkByKey.get(JSON.stringify(names)).value += value;
-                } else {
-                    const link = {
-                        source: indexByKey.get(JSON.stringify([a, d[a]])),
-                        target: indexByKey.get(JSON.stringify([b, d[b]])),
-                        names: names,
-                        value: value,
-                    };
-                    links.push(link);
-                    linkByKey.set(JSON.stringify(names), link);
-                }
-            }
-        }
-
-        return {nodes, links};
-    };
-
-    const chart = (graph,/*list_genres*/) => {
-        const width = 928; // A adapter dynamiquement width = document.getElementById("chart-container").offsetWidth;
-        const height = 720; // A adapter dynamiquement height = document.getElementById("chart-container").offsetHeight;
-
-        const sankey = d3.sankey()
-            .nodeSort(null)
-            .linkSort(null)
-            .nodeWidth(4)
-            .nodePadding(20)
-            .extent([[0, 5], [width, height - 5]]);
-
-        const color = d3.scaleOrdinal(["True"], ["#da4f81"]).unknown("#ccc");
-
-        const svg = d3.create("svg")
-            .attr("viewBox", [0, 0, width, height])
-            .attr("width", width)
-            .attr("height", height)
-            .attr("style", "max-width: 100%; height: auto;");
-
-
-        const {nodes, links} = sankey({
-            nodes: graph.nodes.map(d => Object.create(d)),
-            links: graph.links.map(d => Object.create(d))
+        genres.forEach(genre => {
+            const option = document.createElement('option');
+            option.value = genre;
+            option.text = genre;
+            genreDropdown.appendChild(option);
         });
 
-        svg.append("g")
-            .selectAll("rect")
-            .data(nodes)
-//          .filter(d => list_genres.includes(d.genre))
-            .join("rect")
-            .attr("x", d => d.x0)
-            .attr("y", d => d.y0)
-            .attr("height", d => d.y1 - d.y0)
-            .attr("width", d => d.x1 - d.x0)
-            .append("title")
-            .text(d => `${d.name}\n${d.value.toLocaleString()}`);
-
-        svg.append("g")
-            .attr("fill", "none")
-            .selectAll("g")
-            .data(links) // .filter(d => list_genres.includes(d.names[0])) Might be a issue
-            .join("path")
-            .attr("d", d3.sankeyLinkHorizontal())
-            .attr("stroke", d => color(d.names[0]))
-            .attr("stroke-width", d => d.width)
-            .style("mix-blend-mode", "multiply")
-            .append("title")
-            .text(d => `${d.names.join(" → ")}\n${d.value.toLocaleString()}`);
-
-        svg.append("g")
-            .style("font", "10px sans-serif")
-            .selectAll("text")
-            .data(nodes)
-        //            .filter(d => list_genres.includes(d.genre))
-            .join("text")
-            .attr("x", d => d.x0 < width / 2 ? d.x1 + 6 : d.x0 - 6)
-            .attr("y", d => (d.y1 + d.y0) / 2)
-            .attr("dy", "0.35em")
-            .attr("text-anchor", d => d.x0 < width / 2 ? "start" : "end")
-            .text(d => d.name)
-            .append("tspan")
-            .attr("fill-opacity", 0.7)
-            .text(d => ` ${d.value.toLocaleString()}`);
-
-        return svg.node();
-    };
-
-    /**
-     * Get all the genres from the data
-     * @param data
-     * @returns Set of Genres
-     */
-    function get_genres(data) {
-        let genres = new Set();
-        data.forEach(i => {
-            genres.add(i["genre"]);
-        })
-        return genres;
+        genreDropdown.addEventListener('click', updateChart);
     }
 
-    function createChexboxes(data){
-        // Get list of genres from data
-        let genres = get_genres(data);
-        // Get the body element
-        const checkboxes = document.getElementById("checkboxes");
-        genres.forEach(i => {
-            checkboxes.appendChild(createCheckbox(i));
-            list_genres.push(i);
-        });
-    }
-
-    function createCheckbox(txt){
-        // Create the checkbox
-        const checkbox = document.createElement('input');
-        checkbox.type = "checkbox";
-        checkbox.name = txt;
-        checkbox.value = txt;
-        checkbox.id = txt;
-        checkbox.checked = true;
-        // Create the label
-        const label = document.createElement('label');
-        label.htmlFor = txt;
-        label.appendChild(document.createTextNode(txt));
-        // Add them to the container div
-        const container = document.createElement('div');
-        container.appendChild(checkbox);
-        container.appendChild(label);
-        document.addListener(checkbox, 'click', function(e){
-            e.preventDefault();
-            e.stopPropagation();
-            if (e.target.checked) {
-                list_genres.add(e.target.value);
-            } else {
-                list_genres.delete(e.target.value);
-            }
-            updateChart(graph,list_genres);
-        });
-        return container;
-    }
-
-function updateChart(graph,list_genres) {
-        const chartContainer = d3.select("#chart-container");
-        const sankeyData = chart(graph, list_genres);
-        // chartContainer.node().removeChild() supprimer tout les enfants
-        chartContainer.node().appendChild(sankeyData); // ajouter un enfant
-}
-
-// Example usage:
-    const csvFilePath = "parallel_set_filtered.csv";
-    let list_genres = new Set(); // Set of genres to display, might need to be global
+    const genres = ["Alternative", "Baroque", "Blues", "Country", "Dance", "Disco", "Electro", "Folk", "Funk", "Gospel", "Hip-hop", "House", "Jazz", "Metal", "Other", "Pop", "Punk", "R&B", "Reggae", "Rock", "Souls", "Undefined"]
+    const csvFilePath = "filled_parallel_set.csv";
     d3.csv(csvFilePath).then(function (data) {
-        createChexboxes(data);
+        createCheckboxes(genres);
         const chartContainer = d3.select("#chart-container");
         const graphData = graph(data);
-        // updateChart(graphData); // To add
-        const sankeyData = chart(graphData);  //To remove
-        chartContainer.node().appendChild(sankeyData); //To remove
+        const sankeyData = chart(graphData, genres);
+        chartContainer.node().appendChild(sankeyData);
+    });
+
+
+    // const c = document.getElementById("chart-container");
+    // c.addEventListener('click', function() {
+    //     console.log("click");
+    //     tmp = d3.select(this);
+    //     tmp.attr('fill', 'red');
+    //     console.log(tmp);
+    // });
+    addPathsListeners()
+}
+
+function addPathsListeners() {
+    Array.from(document.getElementsByTagName("svg")).forEach(function(path){
+        path.addEventListener('click', function (e) {
+                console.log("path click");
+                console.log(e);
+            }
+        )
+    })
+}
+
+const graph = (data) => {
+    console.log(data)
+    const keys = data.columns.slice(0, -1);
+    let index = -1;
+    const nodes = [];
+    const nodeByKey = new Map();
+    const indexByKey = new Map();
+    const links = [];
+
+    for (const k of keys) {
+        for (const d of data) {
+            const key = [k, d[k]];
+            if (!nodeByKey.has(JSON.stringify(key))) {
+                const node = {name: d[k]};
+                nodes.push(node);
+                nodeByKey.set(JSON.stringify(key), node);
+                indexByKey.set(JSON.stringify(key), ++index);
+            }
+        }
+    }
+
+    for (let i = 1; i < keys.length; ++i) {
+        const a = keys[i - 1];
+        const b = keys[i];
+        const prefix = keys.slice(0, i + 1);
+        const linkByKey = new Map();
+        for (const d of data) {
+            const names = prefix.map(k => d[k]);
+            const value = d.value || 1;
+            if (linkByKey.has(JSON.stringify(names))) {
+                linkByKey.get(JSON.stringify(names)).value += value;
+            } else {
+                const link = {
+                    source: indexByKey.get(JSON.stringify([a, d[a]])),
+                    target: indexByKey.get(JSON.stringify([b, d[b]])),
+                    names: names,
+                    value: value,
+                };
+                links.push(link);
+                linkByKey.set(JSON.stringify(names), link);
+            }
+        }
+    }
+
+    return {nodes, links};
+};
+
+const chart = (graph, list_genres) => {
+    const width = 1200; // A adapter dynamiquement width = document.getElementById("chart-container").offsetWidth;
+    const height = 720; // A adapter dynamiquement height = document.getElementById("chart-container").offsetHeight;
+
+    const sankey = d3.sankey()
+        .nodeSort(null)
+        .linkSort(null)
+        .nodeWidth(4)
+        .nodePadding(20)
+        .extent([[0, 5], [width, height - 5]]);
+
+    const color = d3.scaleOrdinal(["True"], ["#f34343"]).unknown("#93e0d8");
+
+    const svg = d3.create("svg")
+        .attr("viewBox", [0, 0, width, height])
+        .attr("width", width)
+        .attr("height", height)
+        .attr("style", "max-width: 100%; height: auto;");
+
+
+    const {nodes, links} = sankey({
+        nodes: graph.nodes.map(d => Object.create(d)),
+        links: graph.links.map(d => Object.create(d))
+    });
+
+    svg.append("g")
+        .selectAll("rect")
+        .data(nodes)
+        .join("rect")
+        .attr("x", d => d.x0)
+        .attr("y", d => d.y0)
+        .attr("height", d => d.y1 - d.y0)
+        .attr("width", d => d.x1 - d.x0)
+        .append("title")
+        .text(d => `Y ${d.name}\n${d.value.toLocaleString()}`);
+
+    svg.append("g")
+        .attr("fill", "none")
+        .selectAll("g")
+        .data(links)
+        .join("path")
+        .attr("d", d3.sankeyLinkHorizontal())
+        .attr("stroke", d => color(d.names[0]))
+        .attr("stroke-width", d => d.width)
+        .style("mix-blend-mode", "multiply")
+        .append("title")
+        .text(d => `${d.names.join(" → ")}\n${d.value.toLocaleString()}`)
+
+    svg.append("g")
+        .style("font", "1em sans-serif")
+        .selectAll("text")
+        .data(nodes)
+        .join("text")
+        .attr("x", d => d.x0 < width / 2 ? d.x1 + 6 : d.x0 - 6)
+        .attr("y", d => (d.y1 + d.y0) / 2)
+        .attr("dy", "0.5em")
+        .attr("text-anchor", d => d.x0 < width / 2 ? "start" : "end")
+        .text(d => d.name)
+        .append("tspan")
+        .attr("fill-opacity", 0.7)
+        .text(d => ` ${d.value.toLocaleString()}`);
+
+    // append un autre g pour le on click details?
+
+    return svg.node();
+};
+
+function updateChart() {
+    const csvFilePath = "filled_parallel_set.csv";
+    d3.csv(csvFilePath).then(function (data) {
+        console.log("updateChart");
+        const genreDropdown = document.getElementById('genre-dropdown');
+        const selectedGenres = Array.from(genreDropdown.selectedOptions).map(option => option.value);
+        console.log(selectedGenres);
+        const filtered = data.filter(function(d)
+        {
+            if (selectedGenres.includes(d["genre"])) {
+                return d;
+            }
+        });
+        filtered.columns = data.columns;
+        console.log(typeof data,typeof filtered);
+        const chartContainer = d3.select("#chart-container");
+        const upGraphData = graph(filtered);
+        const upSankeyData = chart(upGraphData, selectedGenres);
+        //remove child
+        chartContainer.node().removeChild(chartContainer.node().lastChild);
+        chartContainer.node().appendChild(upSankeyData);
     });
 }
